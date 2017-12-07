0b07e029376345baf6db7fdeb24c915c392db679
ClaraAmeller <claraameller7@gmail.com>
2017-12-07 16:55:41 +0100

    Merge

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // PACKAGES REQUIRED
 const express = require('express');
-// const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
