ab2b89bcdddbbfe09d1587a344e4c1ef79e38c54
ClaraAmeller <claraameller7@gmail.com>
2017-12-08 12:17:23 +0100

    Auth working

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Ingredient = require('../models/ingredient');
 
 // --- Get ingredients on searcher, autocomplete input --- //
-router.get('/', (req, res, next) => {
+router.get('/ingredients', (req, res, next) => {
     const terms = req.query.terms;
     let findQuery = {};
 
@@ -20,5 +20,21 @@ router.get('/', (req, res, next) => {
         res.json(null);
     }
 });
+
+// --- Get recipes by selected ingredients --- //
+// router.get('/recipes', (req, res, next) => {
+//     const ingredients = req.query.ingredients;
+
+//     console.log('ingredients: ' + JSON.stringify(ingredient));
+//     // let findQuery = {};
+
+//     // findQuery = { name: search };
+//     // Ingredient.find(findQuery, (err, ingredient) => {
+//     //     if (err) {
+//     //         return next(err);
+//     //     }
+//     //     res.json(ingredient);
+//     // });
+// });
 
 module.exports = router;
