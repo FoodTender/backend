c778c7bc57a768b29adb8fddd4db641917cb64f1
ClaraAmeller <claraameller7@gmail.com>
2017-12-08 09:06:38 +0100

    Improved auth, error need to fix err

--- models/user.js
+++ models/user.js
@@ -2,9 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: String,
-    email: String,
-    password: String,
+    username: {
+        type: String,
+        required: [true, 'Username is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
     bookmarks: [
         { type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' }
     ],
@@ -15,5 +24,13 @@ const userSchema = new Schema({
 }, {
         timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
     });
+
+userSchema.methods.asData = function () {
+    return {
+        id: this._id,
+        username: this.username,
+        email: this.email
+    };
+};
 
 module.exports = mongoose.model('User', userSchema);
