ab8d488f42d59b85fd37b64bafa86f6c8afdd848
ClaraAmeller <claraameller7@gmail.com>
2017-12-11 11:11:55 +0100

    Create /recipes WIP

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Ingredient = require('../models/ingredient');
-const Recipe = require('../models/recipe');
+// const Recipe = require('../models/recipe');
 
 // --- Get ingredients on searcher, autocomplete input --- //
 router.get('/ingredients', (req, res, next) => {
@@ -22,30 +22,40 @@ router.get('/ingredients', (req, res, next) => {
     }
 });
 
-// --- Get recipes by selected ingredients --- //
-router.get('/recipes', (req, res, next) => {
-    const ingredients = req.query.ingredients || '';
-    console.log('Ingredients: ' + ingredients);
+// router.get('/ingredients/basics', (req, res, next) => { // For autocompleting chips
+//     Ingredient.find({basic: true}, (err, ingredient) => {
+//         if (err) {
+//             return next(err);
+//         }
+//         console.log(ingredient.name);
+//         res.json(ingredient);
+//     });
+// });
 
-    Ingredient.getIdsFromNames(ingredients.split(','))
-        .then((ingredientIds) => {
-            console.log(ingredientIds);
-            Recipe.find({ 'ingredients.ingredient': { $all: ingredientIds } }, (err, ingredient) => {
-                if (err) {
-                    throw next(err);
-                }
-                console.log(ingredient);
-                res.json(ingredient);
-            });
-            // const filter = {};
-            // Recipe.find(filter).populate('ingredients.ingredient')
-            //     .exec((err, recipes) => {
-            //         if (err) {
-            //             return next(err);
-            //         }
-            //         res.json(recipes);
-            //     });
-        });
-});
+// --- Get recipes by selected ingredients --- //
+// router.get('/recipes', (req, res, next) => {
+//     const ingredients = req.query.ingredients || '';
+//     console.log('Ingredients: ' + ingredients);
+
+//     Ingredient.getIdsFromNames(ingredients.split(','))
+//         .then((ingredientIds) => {
+//             console.log(ingredientIds);
+//             Recipe.find({ 'ingredients.ingredient': { $all: ingredientIds } }, (err, ingredient) => {
+//                 if (err) {
+//                     throw next(err);
+//                 }
+//                 console.log(ingredient);
+//                 res.json(ingredient);
+//             });
+//             // const filter = {};
+//             // Recipe.find(filter).populate('ingredients.ingredient')
+//             //     .exec((err, recipes) => {
+//             //         if (err) {
+//             //             return next(err);
+//             //         }
+//             //         res.json(recipes);
+//             //     });
+//         });
+// });
 
 module.exports = router;
