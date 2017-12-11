8e340eb5758323aefc5e0f4a37c01f1fd4176380
Cristina Moreno <moreno.medran@gmail.com>
2017-12-11 13:17:06 +0100

    solved one forgotten conflict

--- app.js
+++ app.js
@@ -65,11 +65,7 @@ app.use(cookieParser());
 app.use('/', index);
 app.use('/auth', auth);
 app.use('/', ingredients);
-<<<<<<< HEAD
-app.use('/recipes', recipes);
-=======
 app.use('/', recipes);
->>>>>>> develop
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
