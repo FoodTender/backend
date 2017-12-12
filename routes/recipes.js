ebae7f20ad2b58aee47388c7914180b174de9aaf
ClaraAmeller <claraameller7@gmail.com>
2017-12-12 11:57:26 +0100

    Create recipe-detail page

--- routes/recipes.js
+++ routes/recipes.js
@@ -3,6 +3,20 @@ const router = express.Router();
 const Ingredient = require('../models/ingredient');
 const Recipe = require('../models/recipe');
 
+// --- Get clicked recipe --- //
+router.get('/recipes/:id', (req, res, next) => {
+    console.log('recipes-');
+    // let recipeId = req.params.id;
+    // console.log('recipeId: ' + recipeId);
+
+    // Recipe.findById(recipeId, (err, recipe) => {
+    //     if (err) {
+    //         throw next(err);
+    //     }
+    //     console.log(recipe);
+    // });
+});
+
 // --- Get recipes by selected ingredients --- //
 router.get('/recipes', (req, res, next) => {
     let ingredients = req.query.ingredients || '';
@@ -12,17 +26,14 @@ router.get('/recipes', (req, res, next) => {
 
     Ingredient.getIdsFromNames(ingredients.split(','))
         .then((ingredientIds) => {
-            console.log('ingredientsIds: ' + ingredientIds.length);
             // db.recipes.find({ingredients: {$all: ['Potato', 'Egg', 'Cheese', 'Bacon', 'Breadcrumbs']}, ingredients: {$size: 5}})
             Recipe.find({ 'ingredients.ingredient': ingredientIds }, (err, recipe) => {
                 if (err) {
                     throw next(err);
                 }
                 recipe.forEach(doc => {
-                    console.log(doc.ingredients.length);
                     if (doc.ingredients.length <= ingredientIds.length) {
                         resultantRecipes.push(doc);
-                        console.log('doc: ', typeof (doc));
                     }
                 });
                 res.json(resultantRecipes);
