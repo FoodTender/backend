5a7320393d4ef204ed7772e1b9dbb6fc433505e1
ClaraAmeller <claraameller7@gmail.com>
2017-12-13 22:21:43 +0100

    /bookmarks done

--- routes/bookmarks.js
+++ routes/bookmarks.js
@@ -1,24 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const Recipe = require('../models/recipe');
 const User = require('../models/user').User;
 
 router.get('/bookmarks', (req, res, next) => {
-    console.log('route');
-    // User.findOne({ username: req.user.username }, (err, user) => {
-    //     if (err) {
-    //         throw next(err);
-    //     }
-    //     console.log(user);
-    // });
-
     User.findOne({ username: req.user.username }, 'bookmarks')
         .populate('bookmarks')
         .exec(function (err, bookmarks) {
             if (err) {
                 throw next(err);
             }
-            console.log(bookmarks);
             res.json(bookmarks);
         });
 });
