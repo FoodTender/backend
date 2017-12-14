5805614dc8238b2ed76e4f590a6e4f546b785620
ClaraAmeller <claraameller7@gmail.com>
2017-12-14 12:38:20 +0100

    User saves bookmarks WIP: bookmarks repeated

--- routes/bookmarks.js
+++ routes/bookmarks.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user').User;
+const Recipe = require('../models/recipe');
 
 router.get('/bookmarks', (req, res, next) => {
     User.findOne({ username: req.user.username }, 'bookmarks')
@@ -11,6 +12,31 @@ router.get('/bookmarks', (req, res, next) => {
             }
             res.json(bookmarks);
         });
+});
+
+router.put('/bookmarks/:recipeId/add', (req, res, next) => {
+    let recipeId = req.params.recipeId;
+    console.log('route');
+    Recipe.findById(recipeId, (err, recipe) => {
+        if (err) {
+            throw next(err);
+        }
+        console.log(recipe);
+
+        // Add User.bookmarks.push(recipe)
+        User.findOne({ username: req.user.username }, (err, user) => {
+            if (err) {
+                throw next(err);
+            }
+            user.bookmarks.push(recipe._id);
+            user.save((err) => {
+                if (err) {
+                    throw next(err);
+                }
+            });
+            console.log(user);
+        });
+    });
 });
 
 module.exports = router;
